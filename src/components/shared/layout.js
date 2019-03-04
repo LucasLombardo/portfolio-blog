3ebb93c76c1c917d396d52be0fc39a5d4f957386
LucasLombardo <lucasalombardo@gmail.com>
2019-03-04 12:19:25 -0500

    Remove static query from layout
    Remove the static query from layout as it was not being used. In the
    future static queries can be added back in with hooks

--- src/components/shared/layout.js
+++ src/components/shared/layout.js
@@ -1,28 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 import GlobalStyles from '../styles/globalStyles'
 
 const Layout = ({ children }) => (
-    <StaticQuery
-        query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-        render={data => (
-      <>
+    <>
         <GlobalStyles />
         <header></header>
         <main>{children}</main>
         <footer></footer>
-      </>
-        )}
-    />
+    </>
 )
 
 Layout.propTypes = {
