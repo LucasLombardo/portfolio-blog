e44801908066262959db91c2a6a5572d580b4a88
LucasLombardo <lucasalombardo@gmail.com>
2019-03-04 08:53:23 -0500

    Add react-spring and change header keyframes to react-spring
    Add react-spring to the project and replace the janky transform keyframe
    animation in the header component with a react-spring transition.

--- src/components/home/header.js
+++ src/components/home/header.js
@@ -3,6 +3,7 @@ import Svg from "../shared/svg"
 import styled from "styled-components"
 import HeaderBackground from "./headerBackground"
 import { colors } from "../styles/lib/colors"
+import { useSpring, animated } from "react-spring"
 
 const Content = styled.div`
     position: relative;
@@ -15,44 +16,12 @@ const Content = styled.div`
     text-align: center;
     text-shadow: 0 1px 3px rgba(0, 0, 0, 0.05), 0 1px 2px rgba(0, 0, 0, 0.1);
 
-    /* remove margin between header and subheader */
     h1 {
         margin-bottom: 0;
     }
+
     p {
         margin-top: 0;
-    }
-
-    /* scale in and fade text in */
-    animation-name: scale-in;
-    animation-duration: 1.5s;
-    h1, p {
-        opacity: 0;
-        animation-name: fade-in;
-        animation-duration: 0.8s;
-        animation-delay: 0.5s;
-        animation-timing-function: ease-in;
-        animation-fill-mode: forwards;
-    }
-    @keyframes scale-in {
-        0% {
-            transform: scale(1.1) translate3d(0,0,0);
-            opacity: 0.5;
-        }
-        30% {
-            opacity: 1  translateZ(0);
-        }
-        100% {
-            transform: scaleY(1);
-        }
-    }
-    @keyframes fade-in {
-        0% {
-            opacity: 0;
-        }
-        100% {
-            opacity: 1;
-        }
     }
 `
 
@@ -76,18 +45,28 @@ const Chevron = styled.a`
     }
 `
 
-const Header = () => (
-    <header>
-        <HeaderBackground>
-            <Content>
-                <h1 className="fade-in">Lucas Lombardo</h1>
-                <p className="fade-in">Boston based web developer</p>
-            </Content>
-            <Chevron>
-                <Svg name="chevron" />
-            </Chevron>
-        </HeaderBackground>
-    </header>
-)
+const Header = () => {
+    const scaleIn = useSpring({
+        to: { transform: `scaleY(1)` },
+        from: { transform: `scaleY(1.1)` },
+        delay: `100`,
+    })
+
+    return (
+        <header>
+            <HeaderBackground>
+                <animated.div style={scaleIn}>
+                    <Content>
+                        <h1>Lucas Lombardo</h1>
+                        <p className="fade-in">Boston based web developer</p>
+                    </Content>
+                </animated.div>
+                <Chevron>
+                    <Svg name="chevron" />
+                </Chevron>
+            </HeaderBackground>
+        </header>
+    )
+}
 
 export default Header
