1114eba4de9a2ad1430920fde855898bd24dd6dd
LucasLombardo <lucasalombardo@gmail.com>
2019-03-08 19:10:02 -0500

    Add 4th elevation shadow and remove box-shadow
    Add 4th elevation level in elevation.js and remove the box shadow rule
    to allow for use in text-shadows

--- src/components/styles/lib/elevation.js
+++ src/components/styles/lib/elevation.js
@@ -1,7 +1,8 @@
 // Box shadows at various heights, adapted from material design guidelines.
 
 export const elevation = [
-    `box-shadow: inset 0 7px 9px -7px rgba(0,0,0, 0.7)`,
-    `box-shadow: 0 1px 3px rgba(0,0,0, 0.12), 0 1px 2px rgba(0,0,0, 0.24)`,
-    `box-shadow: 0 3px 6px rgba(0,0,0, 0.16), 0 3px 6px rgba(0,0,0, 0.23)`
+    `inset 0 7px 9px -7px rgba(0,0,0, 0.7)`,
+    `0 1px 3px rgba(0,0,0, 0.12), 0 1px 2px rgba(0,0,0, 0.24)`,
+    `0 3px 6px rgba(0,0,0, 0.16), 0 3px 6px rgba(0,0,0, 0.23)`,
+    `0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)`
 ]
