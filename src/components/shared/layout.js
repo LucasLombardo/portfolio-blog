0e77acdd5670a27b679e3b321cbe3ddd3d99323f
LucasLombardo <lucasalombardo@gmail.com>
2019-03-08 19:12:13 -0500

    Add double quotes for consistency

--- src/components/shared/layout.js
+++ src/components/shared/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import GlobalStyles from '../styles/globalStyles'
+import GlobalStyles from "../styles/globalStyles"
 
 const Layout = ({ children }) => (
     <>
