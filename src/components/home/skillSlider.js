a1e621a69e57ab3ccfc3b5e6102b3b68d5a7878e
LucasLombardo <lucasalombardo@gmail.com>
2019-03-23 15:29:10 -0400

    Add component for skill skillSlider
    Add a baseline component to be a skill slider carousel

---
+++ src/components/home/skillSlider.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+
+const SliderWrapper = styled.div`
+    background: red;
+    height: 100px;
+`
+
+const SkillSlider = () => (
+    <SliderWrapper>
+        <p>Hey</p>
+    </SliderWrapper>
+)
+
+export default SkillSlider
--- src/components/home/skills.js
+++ src/components/home/skills.js
@@ -3,16 +3,20 @@ import styled from "styled-components"
 import Container from "../styles/container"
 import { colors } from "../styles/lib"
 import SectionHeader from "../styles/sectionHeader"
+import SkillSlider from "./skillSlider"
 
 const SkillsWrapper = styled.div`
     background: ${ colors.white };
     overflow: auto;
     padding: 3em 0;
+    p {
+        text-align: center;
+        margin-bottom: 2em;
+    }
     .skillDivs {
-        display: flex;
-        div {
-            margin-right: 3em;
-        }
+        height: 140px;
+        border-top: 6px double ${ colors.teal };
+        border-bottom: 6px double ${ colors.teal };
     }
 `
 
@@ -23,26 +27,9 @@ const Skills = () => {
                 <SectionHeader>
                     <h2>Skills</h2>
                 </SectionHeader>
-                <p>(still working on deciding what this section should look like)</p>
+                <p>These are a few of the technologies I enjoy working with:</p>
                 <div className="skillDivs">
-                    <div>
-                        <h4>Proficient</h4>
-                        <ul>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>CSS3</li>
-                            <li>HTML5</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h4>Knowledgeable</h4>
-                        <ul>
-                            <li>Ruby on Rails</li>
-                            <li>Bootstrap</li>
-                            <li>TypeScript</li>
-                            <li>jQuery</li>
-                        </ul>
-                    </div>
+                    <SkillSlider />
                 </div>
             </Container>
         </SkillsWrapper>
